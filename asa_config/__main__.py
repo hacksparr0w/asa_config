f09f705ec2f94cbab8c67b29e5517f223f6056b5
Zdenek Rottenberg <zrottenberg@gmail.com>
2023-08-12 15:25:13 +0200

    chore: fix command rule path

--- asa_config/__main__.py
+++ asa_config/__main__.py
@@ -4,7 +4,7 @@ from textwrap import dedent
 from ._parse.rule import parse_rules
 
 
-RULE_DATA_PATH = Path(__file__).parent.parent / "data" / "rules.txt"
+RULE_DATA_PATH = Path(__file__).parent.parent / "data" / "command_rules.txt"
 
 
 def main():
