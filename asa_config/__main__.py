36879b7b721409f1eeced854281e60d53cfc67a3
hacksparr0w <hacksparr0w@protonmail.com>
2023-12-21 09:12:23 +0100

    chore: progress

--- asa_config/__main__.py
+++ asa_config/__main__.py
@@ -1,8 +1,17 @@
 from ._core import DEFAULT_RULES
+from ._rule import match
+
+from devtools import pprint
 
 
 def main():
-    print(DEFAULT_RULES)
+    # print(DEFAULT_RULES)
+    result = match(
+        ["access-list", "MY_ACL", "extended", "permit", "TCP", "object-group", "GRP_IBMSOBOX", "object-group", "GRP_NET1691403080", "eq", "888", "log"],
+        DEFAULT_RULES
+    )
+
+    pprint(result)
 
 
 if __name__ == "__main__":
--- asa_config/_rule.py
+++ asa_config/_rule.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
-from typing import Any, Union
+from typing import Any, Generator, Union
 
 from pydantic import BaseModel
+
+from ._decision import decision_tree
 
 
 __all__ = (
@@ -22,25 +24,181 @@ __all__ = (
 )
 
 
+class ObjectRuleMatchError(Exception):
+    pass
+
+
+class ObjectRuleNotFoundError(ObjectRuleMatchError):
+    pass
+
+
+class ObjectRuleParameterMatchError(ObjectRuleMatchError):
+    pass
+
+
+class ObjectRulePositionalParameterMatchError(ObjectRuleParameterMatchError):
+    pass
+
+
+class ObjectRuleUnionParameterMatchError(ObjectRuleParameterMatchError):
+    pass
+
+
+class NotEnoughArgumentsError(ObjectRuleParameterMatchError):
+    pass
+
+
+class ObjectRuleMatch(BaseModel):
+    rule: ObjectRule
+    literals: list[str]
+    positionals: dict[str, Any]
+
+
+def _wrap_matcher(function):
+    def wrapped(arguments):
+        try:
+            for item in function(arguments):
+                yield (item, (item[1],), {})
+        except ObjectRuleParameterMatchError:
+            return
+
+    return wrapped
+
+
 class ObjectRule(BaseModel):
     name: str
     parameters: list[ObjectRuleParameter]
+
+    def match(
+        self,
+        arguments: list[str],
+        root: bool = False
+    ) -> tuple[ObjectRuleMatch, list[str]]:
+        literals = []
+        positionals = {}
+        generators = [
+            _wrap_matcher(parameter.match)
+            for parameter in self.parameters
+        ]
+
+        decisions = list(decision_tree(generators)(arguments))
+
+        if not decisions:
+            raise ObjectRuleParameterMatchError
+
+        if root:
+            for decision in decisions:
+                steps = [s for s, _ in decision]
+                inner = [i for i in steps]
+                from devtools import pprint
+                print(inner)
+
+        decision = decisions[0]
+
+        for matches, _ in decision:
+            for item in matches:
+                if isinstance(item, ObjectRuleLiteralParameterMatch):
+                    literals.append(item.value)
+                elif isinstance(item, ObjectRulePositionalParameterMatch):
+                    positionals[item.name] = item.value
+                else:
+                    raise RuntimeError
+
+        return (
+            ObjectRuleMatch(
+                rule=self,
+                literals=literals,
+                positionals=positionals
+            ),
+            arguments
+        )
+
+
+class ObjectRuleLiteralParameterMatch(BaseModel):
+    value: str
+
+
+class ObjectRulePositionalParameterMatch(BaseModel):
+    name: str
+    value: Any
+
+
+ObjectRuleParameterMatch = Union[
+    ObjectRuleLiteralParameterMatch,
+    ObjectRulePositionalParameterMatch
+]
 
 
 class ObjectRuleLiteralParameter(BaseModel):
     value: str
 
+    def match(
+        self,
+        arguments: list[str]
+    ) -> Generator[
+        tuple[
+            list[ObjectRuleLiteralParameterMatch],
+            list[str]
+        ],
+        None,
+        None
+    ]:
+        try:
+            value, *arguments = arguments
+        except ValueError:
+            raise NotEnoughArgumentsError
+
+        if value.lower() != self.value.lower():
+            raise ObjectRuleParameterMatchError
+
+        yield [ObjectRuleLiteralParameterMatch(value=value)], arguments
+
 
 class ObjectRulePositionalParameterObjectValue(ObjectRule):
-    pass
+    def match(
+        self,
+        arguments: list[str]
+    ) -> tuple[ObjectRuleMatch, list[str]]:
+        try:
+            value, *arguments = arguments
+        except ValueError:
+            raise NotEnoughArgumentsError
+
+        if value.lower() != self.name.lower():
+            raise ObjectRuleParameterMatchError
+
+        return super().match(arguments)
 
 
 class ObjectRulePositionalParameterStringValue(BaseModel):
-    pass
+    def match(
+        self,
+        arguments: list[str]
+    ) -> tuple[str, list[str]]:
+        try:
+            value, *arguments = arguments
+        except ValueError:
+            raise NotEnoughArgumentsError
+
+        return value, arguments
 
 
 class ObjectRulePositionalParameterIntegerValue(BaseModel):
-    pass
+    def match(
+        self,
+        arguments: list[str]
+    ) -> tuple[int, list[str]]:
+        try:
+            value, *arguments = arguments
+        except ValueError:
+            raise NotEnoughArgumentsError
+
+        try:
+            value = int(value)
+        except ValueError:
+            raise ObjectRulePositionalParameterMatchError
+
+        return value, arguments
 
 
 ObjectRulePositionalParameterValue = Union[
@@ -54,9 +212,71 @@ class ObjectRulePositionalParameter(BaseModel):
     name: str
     value: list[ObjectRulePositionalParameterValue]
 
+    def match(
+        self,
+        arguments: list[str]
+    ) -> Generator[
+        tuple[
+            list[ObjectRulePositionalParameterMatch],
+            list[str]
+        ],
+        None,
+        None
+    ]:
+        error = None
+
+        for value in self.value:
+            try:
+                result, remaining_arguments = value.match(arguments)
+
+                yield (
+                    [
+                        ObjectRulePositionalParameterMatch(
+                            name=self.name,
+                            value=result
+                        )
+                    ],
+                    remaining_arguments
+                )
+            except ObjectRuleParameterMatchError as exception:
+                error = exception
+
+        if error:
+            raise error
+
 
 class ObjectRuleUnionParameter(BaseModel):
     members: list[list[ObjectRuleParameter]]
+
+    def match(
+        self,
+        arguments: list[str]
+    ) -> Generator[
+        tuple[
+            list[ObjectRuleParameterMatch],
+            list[str]
+        ],
+        None,
+        None
+    ]:
+        for member in self.members:
+            if member == []:
+                yield [], arguments
+                continue
+
+            generators = [
+                _wrap_matcher(parameter.match)
+                for parameter in member
+            ]
+
+            for decision in decision_tree(generators)(arguments):
+                remaining_arguments = None
+                result = []
+
+                for matches, remaining_arguments in decision:
+                    result.extend(matches)
+
+                yield result, remaining_arguments
 
 
 ObjectRuleParameter = Union[
@@ -66,15 +286,26 @@ ObjectRuleParameter = Union[
 ]
 
 
-class ObjectRuleMatch(BaseModel):
-    rule: ObjectRule
-    literals: list[str]
-    positionals: dict[str, Any]
+def _find_rules(
+    name: str,
+    rules: list[ObjectRule]
+) -> list[ObjectRule]:
+    return [rule for rule in rules if rule.name == name]
 
 
-def match(
-    arguments: list[str],
-    rules: list[ObjectRule],
-    greedy: bool = True
-) -> tuple[ObjectRuleMatch, list[str]]:
-    pass
+def match(arguments: list[str], rules: list[ObjectRule]) -> ObjectRuleMatch:
+    name, *arguments = arguments
+    candidates = _find_rules(name, rules)
+
+    if not candidates:
+        raise ObjectRuleNotFoundError(name)
+
+    error = None
+
+    for candidate in candidates:
+        try:
+            return candidate.match(arguments, root=True)
+        except ObjectRuleMatchError as exception:
+            error = exception
+
+    raise error
